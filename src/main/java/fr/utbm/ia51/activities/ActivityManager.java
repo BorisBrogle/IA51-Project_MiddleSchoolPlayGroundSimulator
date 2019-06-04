649fb05b59537224e7d9301aee44951cecf27373
BorisB <boris.brogle@gmail.com>
2019-06-04 23:40:25 +0200

    Activities will now have a higher chance of appearing everytime they are selected

--- src/main/java/fr/utbm/ia51/activities/ActivityManager.java
+++ src/main/java/fr/utbm/ia51/activities/ActivityManager.java
@@ -7,6 +7,8 @@ import fr.utbm.ia51.Globals;
 public class ActivityManager {
 	HashMap<ActivityType, Double> activityChances = new HashMap<>();
 	int nbActivities;
+	double maxWeight = 0.9;
+	double growthFactor = 5.0;
 	
 	public ActivityManager() {
 		nbActivities = ActivityType.values().length;
@@ -20,7 +22,7 @@ public class ActivityManager {
 	
 	
 	/*
-	 * Returns an activity chosen randomly between all the activities using their weights
+	 * Returns an activity chosen randomly between all the activities using their weights, and raise its weight
 	 * @return: activityType that has been chosen
 	 */
 	public ActivityType chooseActivity() {
@@ -33,9 +35,69 @@ public class ActivityManager {
 			activity++;
 			totalWeights += activityChances.get(activities[activity]);
 		} while(totalWeights <= rnd && activity < nbActivities-1);
-		
+
+		raiseActivity(activities[activity]);
+		printActivitiesAndWeights(); //I let it print for the moment, you can comment it if you want
 		return activities[activity];
 	}
 	
+	
+	/*
+	 * Just used to print the different activities and their chances of appearing
+	 */
+	public void printActivitiesAndWeights() {
+		System.out.println("\nPrinting the activity chances:");
+		for (HashMap.Entry<ActivityType, Double> entry : activityChances.entrySet()) {
+			ActivityType key = entry.getKey();
+			Double value = entry.getValue();
+		    System.out.format("Activity %s, weight %.5f\n", key, value);
+		}
+		System.out.println("----\n");
+	}
+	
+	
+	/*
+	 * Function used to raise an activity chance of appearing. 
+	 * It also lowers the chances for the other activities to appear.
+	 * @param a: activity that we want to have a higher chance of appearing
+	 */
+	private void raiseActivity(ActivityType a) {
+		double activityWeight = this.activityChances.get(a);
+		double x = raiseFunctionInvert(activityWeight);
+		double newWeight = raiseFunction(x+1);
+		double diff = newWeight - activityWeight;
+		
+		double totalWeights = 0;
+		for (ActivityType activity : ActivityType.values()) { 
+			if(activity != a) {
+				double weight = activityChances.get(activity);
+				double newDivisedWeight = weight-diff/nbActivities;
+				this.activityChances.put(activity, newDivisedWeight);
+				if(newDivisedWeight < 0.0) {
+					this.activityChances.put(activity, 0.0);
+				}
+				totalWeights += newDivisedWeight;
+			}
+		}
+		this.activityChances.put(a, 1.0-totalWeights);
+	}
+	
+	
+	/*
+	 * Mathematical function that returns y, such as f(x) = y, with f(x)=-growthFactor/(x+growthFactor)+maxWeight;
+	 * @param x
+	 */
+	private double raiseFunction(double x) {
+		return -growthFactor/(x+growthFactor)+maxWeight;
+	}
+	
+	
+	/*
+	 * Mathematical function that returns x, such as f(x) = y, with f(x)=-growthFactor/(x+growthFactor)+maxWeight;
+	 * @param y
+	 */
+	private double raiseFunctionInvert(double y) {
+		return -growthFactor/(y-maxWeight)-growthFactor;
+	}
 	
 }
