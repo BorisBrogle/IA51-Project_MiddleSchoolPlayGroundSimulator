2a64413f903874fed876c1c89636645232d11117
BorisB <boris.brogle@gmail.com>
2019-06-05 13:06:19 +0200

    Added LOOP_DURATION in globals

--- src/main/java/fr/utbm/ia51/Globals.java
+++ src/main/java/fr/utbm/ia51/Globals.java
@@ -6,4 +6,6 @@ import tools.Chrono;
 public class Globals {
 	public static PseudoRandomGenerator randomGenerator = new PseudoRandomGenerator();
 	public static Chrono chrono = new Chrono();
+	
+	public static long LOOP_DURATION = 10; // Duration between each steps, in milliseconds
 }
