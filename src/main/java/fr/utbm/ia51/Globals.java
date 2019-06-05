169cea9d27a1e77aa998ee9ab1723f86aa1aa436
BorisB <boris.brogle@gmail.com>
2019-06-05 12:47:01 +0200

    Added a chrono. With it, the loop_duration is now good.

--- src/main/java/fr/utbm/ia51/Globals.java
+++ src/main/java/fr/utbm/ia51/Globals.java
@@ -1,7 +1,9 @@
 package fr.utbm.ia51;
 
 import fr.utbm.ia51.tools.PseudoRandomGenerator;
+import tools.Chrono;
 
 public class Globals {
 	public static PseudoRandomGenerator randomGenerator = new PseudoRandomGenerator();
+	public static Chrono chrono = new Chrono();
 }
