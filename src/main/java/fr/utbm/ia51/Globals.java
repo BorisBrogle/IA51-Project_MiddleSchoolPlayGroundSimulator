2ad0bf5fbeb010c8fb0393fc1602dfd28bc7ef93
BorisB <boris.brogle@gmail.com>
2019-06-11 22:33:10 +0200

    Agents are now moving inside of their respective activities

--- src/main/java/fr/utbm/ia51/Globals.java
+++ src/main/java/fr/utbm/ia51/Globals.java
@@ -11,7 +11,9 @@ public class Globals {
 	public static final int HEIGHT = 720; // Width of our map, and of our base window
 	
 	public static long LOOP_DURATION = 100; // Duration between each steps, in milliseconds
-	public static long ACTIVITY_CHANGE_FREQUENCY = 3600; // All the agents will change activity every 5 seconds on average
+	
+	public static long ACTIVITY_CHANGE_FREQUENCY = 3600; // All the agents will change activity every x seconds on average
+	public static long IMMOBILITY_TIME = 5; // The agents will stay motionless for 5 seconds max on average
 	
 	public static boolean SHOW_FORCE_VECTOR = true; // If we should print the force vector of the agents
 	
