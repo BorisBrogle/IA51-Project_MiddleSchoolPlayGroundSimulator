a7a13b61a7b68287ebf8aad104d6f3a93dcc089c
YannPoncet <yann.poncetpro@gmail.com>
2019-06-12 10:40:19 +0200

    Weird fix (replace with Head version caused error)

--- src/main/java/fr/utbm/ia51/Globals.java
+++ src/main/java/fr/utbm/ia51/Globals.java
@@ -20,7 +20,4 @@ public class Globals {
 	public static boolean SHUT_DOWN_SIGNAL = false; // Will be set to true when the GUI is closed, kills all the agents
 	
 	public static boolean NIUT_NIUT_LES_CHECKBOX = true;
-	
-	public static final int WIDTH = 1280;
-	public static final int HEIGHT = 720;
 }
