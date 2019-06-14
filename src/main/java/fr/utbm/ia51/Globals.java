d2c282bc50dfd6ee053409b0a6a7edf9ac4fea68
Camille <camille.brondel@utbm.fr>
2019-06-14 14:53:05 +0200

    Stat (activity)

--- src/main/java/fr/utbm/ia51/Globals.java
+++ src/main/java/fr/utbm/ia51/Globals.java
@@ -8,7 +8,7 @@ public class Globals {
 	public static Chrono chrono = new Chrono();
 	
 	
-	public static final int NB_AGENTS = 1;
+	public static final int NB_AGENTS = 3;
 	
 	public static final int WIDTH = 1280; // Height of our map, and of our base window
 	public static final int HEIGHT = 720; // Width of our map, and of our base window
@@ -32,4 +32,24 @@ public class Globals {
 	public static boolean SHOW_STAT = false; // If we should show the statistics
 	public static int TOT_SET_TARGET = 0;
 	public static int TOT_REACHED_TARGET = 0;
+	
+	//STAT FOR EACH ACTIVITIES
+	public static int NB_SET_BASKETBALL = 0;
+	public static int NB_REACHED_BASKETBALL = 0;
+	public static int NB_SET_TENNISTABLE = 0;
+	public static int NB_REACHED_TENNISTABLE = 0;
+	public static int NB_SET_SOCCER = 0;
+	public static int NB_REACHED_SOCCER = 0;
+	public static int NB_SET_BENCH = 0;
+	public static int NB_REACHED_BENCH = 0;
+	public static int NB_SET_LIBRARY = 0;
+	public static int NB_REACHED_LIBRARY = 0;
+	public static int NB_SET_CLASSLINE = 0;
+	public static int NB_REACHED_CLASSLINE = 0;
+	public static int NB_SET_FOREST = 0;
+	public static int NB_REACHED_FOREST = 0;
+	public static int NB_SET_TOILET = 0;
+	public static int NB_REACHED_TOILET = 0;
+	public static int NB_SET_FIGHT = 0;
+	public static int NB_REACHED_FIGHT = 0;
 }
